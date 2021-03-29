6f5de3c189be633dc92b3f93bd6e6b8e650f89da
clay-jay <youkozel@gmail.com>
2021-03-29 11:14:17 +0300

    savepoint

--- src/components/threeSixtyViewer.js
+++ src/components/threeSixtyViewer.js
@@ -1,6 +1,6 @@
-import React, { createRef, useEffect, useState } from "react"
+import React, { createRef, useEffect, useRef, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import UseEventListener from './eventListener'
+import UseEventListener from "./eventListener"
 
 export default function ThreeSixtyViewer() {
   //запрос для получения base64 изображений (width is fixed = 640px)
@@ -36,163 +36,134 @@ export default function ThreeSixtyViewer() {
     }))
     .sort((a, b) => a.id - b.id)
   //объявляем переменные
-  const canvasRef = createRef()
-  const loadedImages = []
-  const [startX, setStartX] = useState(null)
+  const canvasRef = useRef  ()
+  const [loadedImages, setLoadedImages] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [startX, setStartX] = useState(0)
   const [endX, setEndX] = useState(null)
-  const [currentImage, setCurrentImage] = useState(0)
+  const [currentImage, setCurrentImage] = useState(0)   
   const [animation, setAnimation] = useState(null)
   const [newFrame, setNewFrame] = useState(0)
 
   useEffect(() => {
-      const canvas = canvasRef.current
-      canvas.width = canvas.clientWidth
-      canvas.height = canvas.clientHeight 
-      //const context = canvas.getContext("2d")
-      //canvas.width = canvas.clientWidth
-      //canvas.height = canvas.clientHeight 
-    //   var image = new Image()
-    //   image.src = cleanData[0].base64
-    //   image.onload = () =>{
-    //       console.log('loaded')
-    //       context.drawImage(image, 0, 0)
-    //   } 
-
-    //загружаем изображения
+    const canvas = canvasRef
     loadImages()
-    drawImage(5)
-    // canvas.addEventListener("mousedown", handleMouseDown, false);
-    // canvas.addEventListener("touchstart", handleTouchStart, false);
-    // canvas.addEventListener("mousemove", handleMouseMove, false);
-    // canvas.addEventListener("touchmove", handleTouchMove, false);
-    // canvas.addEventListener("mouseup", handleMouseUp, false);
-    // canvas.addEventListener("touchend", handleMouseUp, false);
+    //объявляем хендлеров
+    // window.addEventListener("mousedown", handleMouseDown)
+    // window.addEventListener("touchstart", handleTouchStart)
+    // window.addEventListener("mousemove", handleMouseMove)
+    // window.addEventListener("touchmove", handleTouchMove)
+    // window.addEventListener("mouseup", handleMouseUp)
+    // window.addEventListener("touchend", handleMouseUp)
 
-    return () => {
-        //const canvas = canvasRef.current
-
-        // Remove all event listeners before the component gets removed from the page.
-        // canvas.removeEventListener("mousedown", handleMouseDown, false);
-        // canvas.removeEventListener("touchstart", handleTouchStart, false);
-        // canvas.removeEventListener("mousemove", handleMouseMove, false);
-        // canvas.removeEventListener("touchmove", handleTouchMove, false);
-        // canvas.removeEventListener("mouseup", handleMouseUp, false);
-        // canvas.removeEventListener("touchend", handleMouseUp, false);
-    }
-  })
-  
+    // return (
+    //   () => window.removeEventListener("mousedown", handleMouseDown),
+    //   window.removeEventListener("touchstart", handleTouchStart),
+    //   window.removeEventListener("mousemove", handleMouseMove),
+    //   window.removeEventListener("touchmove", handleTouchMove),
+    //   window.removeEventListener("mouseup", handleMouseUp),
+    //   window.removeEventListener("touchend", handleMouseUp)
+    // )
+  }, [])
   //не справился с управлением Promice
+
   const loadImages = () => {
     const images = cleanData
-    images.forEach((image) => {
-        const img = new Image()
-        img.src = image.base64
-        loadedImages.push(img)
-        if(loadedImages.length === images.length){
-            console.log('images loaded')
-            return
-        } else{
-            drawLoadingBar(
-                (loadedImages.length * 100)/ images.length
-            )
-        }
+    if (loadedImages.length === images.length) {
+      drawImage(0)
+      return
+    }
+    images.forEach(image => {
+      const img = new Image()
+      img.src = image.base64
+      loadedImages.push(img)
+      console.log(loadedImages)
     })
- }
- 
- //отрисовка прогрессбара в канвасе
- const drawLoadingBar = (progress) => {
+    console.log("loaded")
+    drawImage(0)
+  }
+  //отрисовка прогрессбара в канвасе
+  const drawLoadingBar = progress => {
     const canvas = canvasRef.current
     const context = canvas.getContext("2d")
     const barWidth = Math.round(window.innerWidth / 5)
     const barHeight = Math.round(barWidth / 10)
     const barPosX = (canvas.width - barWidth) / 2
     const barPosY = (canvas.height - barHeight) / 2
-    
+
     context.fillStyle = "#0096d6"
     context.clearRect(0, 0, canvas.width, canvas.height)
     context.fillRect(barPosX, barPosY, barWidth, barHeight)
 
-    const fillVal = Math.min(Math.max(progress / 100, 0), 1);
+    const fillVal = Math.min(Math.max(progress / 100, 0), 1)
     context.fillRect(
       barPosX + 1,
       barPosY + 1,
       fillVal * (barWidth - 2),
       barHeight - 2
-    );
- }
+    )
+  }
 
- const drawImage = (frame) => {
+  const drawImage = frame => {
     const canvas = canvasRef.current
     const context = canvas.getContext("2d")
     context.clearRect(0, 0, canvas.width, canvas.height)
     const newImage = loadedImages[frame]
-    context.drawImage(newImage, 0, 0);
+    context.drawImage(newImage, 0, 0)
     setCurrentImage(frame)
-}
+  }
 
-const handleMouseDown = (e) => {
-    setStartX(e.clientX)
-}
+  function handleMouseDown(e) {  
+    setStartX(e.pageX)
+    console.log(startX)
+  }
 
-const handleTouchStart = (e) => {
-    setStartX(e.touches[0].clientX)
-}
+  const handleTouchStart = e => {
+    setStartX(e.touches[0].pageX)
+  }
 
-const handleMouseMove = (e) => {
+  const handleMouseMove = e => {
     if (startX !== null) {
-        const delta = e.clientX - (!endX ? startX : endX)
-        setStartX(e.clientX)
+      const delta = e.pageX - (!endX ? startX : endX)
+      setStartX(e.pageX)
+      console.log('move: ' + startX)
 
-        let startingFrame = currentImage
-        if (currentImage === loadedImages.length - 1) {
-            startingFrame = 0
-        } else if (currentImage === 0) {
-            startingFrame = loadedImages.length -1
-        }
-        
-        let moveFrame = startingFrame
-        if (delta > 0) {
-            moveFrame = startingFrame + 1
-        }
-        
-        setNewFrame(Math.min(
-            Math.max(moveFrame, 0),
-            loadedImages.length - 1
-        )) 
+      let startingFrame = currentImage
+      if (currentImage === loadedImages.length - 1) {
+        startingFrame = 0
+      } else if (currentImage === 0) {
+        startingFrame = loadedImages.length - 1
+      }
 
-        if (animation === null) {
-            setAnimation(requestAnimationFrame(animationFrame))
-        }
+      let moveFrame = startingFrame
+      if (delta > 0) {
+        moveFrame = startingFrame + 1
+      }
+
+      setNewFrame(Math.min(Math.max(moveFrame, 0), loadedImages.length - 1))
+
+    //   if (animation === null) {
+    //     setAnimation(requestAnimationFrame(animationFrame))
+    //   }
     }
-}
-const animationFrame = () => {
-    drawImage(newFrame)
-    setAnimation(requestAnimationFrame(animationFrame))
-}
+  }
+//   const animationFrame = () => {
+//     drawImage(newFrame)
+//     setAnimation(requestAnimationFrame(animationFrame))
+//   }
 
-const handleTouchMove = (e) => handleMouseMove(e.touches[0])
+  const handleTouchMove = e => handleMouseMove(e.touches[0])
 
-const handleMouseUp = () => {
+  const handleMouseUp = () => {
     setStartX(null)
     setEndX(null)
-    animation && cancelAnimationFrame(animation)
+    //animation && cancelAnimationFrame(animation)
     setAnimation(null)
-}
-
-  //объявляем хендлеров
-  UseEventListener("mousedown", handleMouseDown)
-  UseEventListener("touchstart", handleTouchStart)
-  UseEventListener("mousemove", handleMouseMove)
-  UseEventListener("touchmove", handleTouchMove)
-  UseEventListener("mouseup", handleMouseUp)
-  UseEventListener("touchend", handleMouseUp)
+  }
 
   return (
     <div className="canvasContainer">
-      <canvas width='640' height='333' ref={canvasRef}>
-
-      </canvas>
-      <script src='threeSixtyViewer.js'/>
+      <canvas width="640" height="333" ref={canvasRef} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onMouseUp={handleMouseUp}></canvas>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import ThreeSixtyViewer from "../components/threeSixtyViewer"
 
+
 const IndexPage = () => (
   <ThreeSixtyViewer />
 )
