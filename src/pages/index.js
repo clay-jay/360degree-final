e9461e40449b6fa849a1425d28a6793434501c9e
clay-jay <youkozel@gmail.com>
2021-04-08 12:02:15 +0300

    working zipHandler

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react"
+import ZipHandler from '../components/zipHandler'
 import Test from "../components/test"
 import TestV2 from "../components/test2"
 import ThreeSixtyViewer from "../components/threeSixtyViewer"
 
 
 const IndexPage = () => (
-  <TestV2 />
+  <ZipHandler />
 )
 
 export default IndexPage
